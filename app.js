c97c6912b8aee15a5139e74fffca66254d2e7436
Rishi-121 <dashrushikesh1121@gmail.com>
2021-08-15 22:39:38 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const multiMailer = require("node-multi-mailer");
+const path = require("path");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+/// multiMailer configurations start ///
+// multiMailer.configuration({
+//     senderEmail : "no-reply@webizysolutions.com",
+//     senderName : "Webizy IT Solutions",
+//     sendGridApiKay : "PASTE_YOUR_SENDGRID_API_KEY",
+//     replyTo : "support@webizysolutions.com"   /// else default will be no-reply@webizysolutions.com
+// });
+
+/////// For example sending email template with data //////
+
+let emailFolder = path.join(__dirname, "email");
+
+multiMailer.configuration({
+  senderEmail: "info@webmandi.co.in",
+  senderName: "Webizy IT Solutions",
+  sendGridApiKay: process.env.SENDGRID_API_KEY,
+  replyTo: "18cse060.hrushikeshdas@giet.edu", /// else default will be no-reply@webizysolutions.com
+  templateFolderPath: emailFolder,
+});
+
+/// mailer configurations end ///
+
+console.log(emailFolder);
+/// Now you can use it any where inside you project ///
+
+app.get("/", async (req, res, next) => {
+  await multiMailer.sendTextEmail(
+    "developer.shiv2020@gmail.com",
+    " my subject testing ",
+    "Thanks for sending email with node-multi-mailer"
+  );
+
+  return res.status("All Works !!!");
+});
+
+app.get("/example2", async (req, res) => {
+  let data = {
+    firstName: "shiv",
+    lastName: "kumar",
+  };
+
+  await multiMailer.sendEjsTemplateWithData(
+    "dashrushikesh1121@gmail.com",
+    "Tesing of template",
+    "login.ejs",
+    data
+  );
+
+  return res.send(
+    "Email sent to your template with the value bind with 'data' object "
+  );
+});
+
+/**
+ * Open you browser and open
+ *
+ * http://localhost:3000/
+ *
+ *  You will get an email in your inbox
+ *
+ */
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Server is running on port : ${port}`));
